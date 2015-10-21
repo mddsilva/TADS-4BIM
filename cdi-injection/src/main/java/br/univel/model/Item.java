b8320da4621d6a8e29887a03ba367ff8b2ce3e28
Maycon Silva <mayconddsilva@gmail.com>
2015-10-21 20:17:24 -0200

    Foram criadas as propriedades value e limit para o POJO do Item e implemente o método toString().

--- cdi-injection/src/main/java/br/univel/model/Item.java
+++ cdi-injection/src/main/java/br/univel/model/Item.java
@@ -1,5 +1,12 @@
 package br.univel.model;
 
-public class Item
-{
+public class Item {
+	private int value;
+	private int limit;
+
+	@Override
+	public String toString() {
+		return super.toString()
+				+ String.format(" [Value=%d, Limit=%d]", value, limit);
+	}
 }
