73a2488ad6ad9db95c09c7b9f18618e63830d2eb
Maycon Silva <mayconddsilva@gmail.com>
2015-10-21 20:33:13 -0200

    Foi adicionado a implementação da classe DefaultItemDao aonde  cria uma lista de itens e retorna uma lista fixa de itens.

--- cdi-injection/src/main/java/br/univel/dao/DefaultItemDao.java
+++ cdi-injection/src/main/java/br/univel/dao/DefaultItemDao.java
@@ -1,5 +1,19 @@
 package br.univel.dao;
 
-public class DefaultItemDao
-{
+import java.util.ArrayList;
+import java.util.List;
+
+import br.univel.model.Item;
+
+public class DefaultItemDao implements ItemDao {
+
+    @Override
+    public List<Item> fetchItems() {
+        List<Item> results = new ArrayList<Item>();
+        results.add(new Item(34, 7));
+        results.add(new Item(4, 37));
+        results.add(new Item(24, 19));
+        results.add(new Item(89, 32));
+        return results;
+    }
 }
