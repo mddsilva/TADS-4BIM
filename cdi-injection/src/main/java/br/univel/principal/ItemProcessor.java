b49dc499821779bb09ed4fd6404c95beadb25d92
Maycon Silva <mayconddsilva@gmail.com>
2015-10-21 20:29:25 -0200

    Foi realizada a implementação da classe aonde foi criado um método chamado execute e uma propriedade que vai injetar os beans e executará o processo

--- cdi-injection/src/main/java/br/univel/principal/ItemProcessor.java
+++ cdi-injection/src/main/java/br/univel/principal/ItemProcessor.java
@@ -1,5 +1,24 @@
 package br.univel.principal;
 
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.univel.dao.ItemDao;
+import br.univel.model.Item;
+
+@Named
+@RequestScoped
 public class ItemProcessor
 {
+	private ItemDao itemDao;
+
+    public void execute() {
+        List<Item> items = itemDao.fetchItems();
+        for (Item item : items) {
+            System.out.println("Found item " + item);
+        }
+    }
+	
 }
