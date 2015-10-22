df6edd19a9ad263a750e92bf07c1d95206b7df11
Maycon Silva <mayconddsilva@gmail.com>
2015-10-22 21:36:13 -0200

    Foi adicioando a implementação do metodo fetchItems() na classe AnotherItemDao.

--- cdi-injection/src/main/java/br/univel/dao/AnotherItemDao.java
+++ cdi-injection/src/main/java/br/univel/dao/AnotherItemDao.java
@@ -1,5 +1,16 @@
 package br.univel.dao;
 
-public class AnotherItemDao
-{
+import java.util.ArrayList;
+import java.util.List;
+
+import br.univel.model.Item;
+
+public class AnotherItemDao implements ItemDao {
+
+    @Override
+    public List<Item> fetchItems() {
+        List<Item> results = new ArrayList<Item>();
+        results.add(new Item(99, 9));
+        return results;
+    }
 }
